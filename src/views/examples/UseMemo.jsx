563240afe3fc4cec175b4cbadfeb64ca35f70655
oliveiradiney <dineyoliveira1999@gmail.com>
2022-01-09 00:24:39 -0300

    useRef 01

--- src/views/examples/UseMemo.jsx
+++ src/views/examples/UseMemo.jsx
@@ -6,7 +6,7 @@ const UseMemo = (props) => {
         <div className="UseMemo">
             <PageTitle
                 title="Hook UseMemo"
-                subtitle="Retorna um valor memoizado!"
+                subtitle="Retorna um valor memorizado!"
             />
         </div>
     )
