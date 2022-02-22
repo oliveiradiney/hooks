20d8d582caf1c39027acd2e19d3a9841a3e11eac
oliveiradiney <dineyoliveira1999@gmail.com>
2022-02-21 22:21:42 -0300

    useContext e useReducer

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,15 +1,62 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+const initialState = {
+    cart: [],
+    products: [],
+    user: null,
+    // foco...
+    number: 0
+}
+
+//Pegar o estado atual e para cada ação que for acontecendo evoluir o estado alterando algum atributo
+function reducer(state, action){
+    switch(action.type){
+        case 'numberAdd2':
+            return {...state, number: state.number + 2}
+        case 'login':
+            return {...state, user: { name: action.payload}}
+        default:
+            return state
+
+    }
+}
+
+
 const UseReducer = (props) => {
+
+    const [state, dispatch] = useReducer(reducer, initialState)
     return (
-        <div className="UseReducer">
-            <PageTitle
-                title="Hook UseReducer"
-                subtitle="Uma outra forma de ter estado em componentes funcionais!"
-            />
+      <div className="UseReducer">
+        <PageTitle
+          title="Hook UseReducer"
+          subtitle="Uma outra forma de ter estado em componentes funcionais!"
+        />
+
+        <div className="center">
+          {state.user ? (
+            <span className="text">{state.user.name}</span>
+          ) : (
+            <span className="text">Sem Usuário</span>
+          )}
+
+          <span className="text">{state.number}</span>
+          <div>
+            <button
+              className="btn"
+              onClick={() => dispatch({ type: "login", payload: 'Maria'})}
+            >Login
+            </button>
+            <button
+              className="btn"
+              onClick={() => dispatch({ type: "numberAdd2" })}
+            >
+              +2
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default UseReducer
